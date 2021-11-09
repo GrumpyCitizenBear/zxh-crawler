b4a2f66baa233a43c4744793cf7d162b6ced1bd0
GrumpyCitizenBear <1049378767@qq.com>
2021-11-09 09:03:28 +0800

    搭建项目

---
+++ src/main/java/com/github/hcsp/Main.java
@@ -0,0 +1,26 @@
+package com.github.hcsp;
+
+import com.sun.imageio.stream.CloseableDisposerRecord;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //http client apache
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://sina.cn");
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            EntityUtils.consume(entity);
+        }
+
+    }
+}
